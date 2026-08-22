c452837a34ee1bc343d612db0b54bbcabd922e1d
Dinesh Patil <66240036+dineshpatilcloud@users.noreply.github.com>
2026-08-22 17:45:55 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 4.45.0, < 4.49.0"
+      #version = ">= 4.45.0, < 4.49.0"
+      version = "4.80.0"
     }
   }
 
@@ -14,5 +15,5 @@ provider "azurerm" {
   features {
     
   }
-  subscription_id = "32575352-315d-4a79-8163-5efbd5c09901"
+  #subscription_id = "32575352-315d-4a79-8163-5efbd5c09901"
 }
